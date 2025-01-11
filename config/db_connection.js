b91ffe727c650ad2fd07918d5ec9ef4b57065512
Rsoniie <roshansoniee@gmail.com>
2025-01-11 14:33:46 +0530

    All Done

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
 import { DB_Name } from '../constants.js';
 
-const MONGO_URI = 'mongodb://localhost:27017/koinx'
+// const MONGO_URI = 'mongodb://localhost:27017/koinx'
+const MONGO_URI = process.env.MONGODB_URI;
 const connectDB = async () => {
   try {
-    const connectioninstance = await mongoose.connect(MONGO_URI);
+    const connectioninstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`);
     console.log('MongoDB connection established successfully', connectioninstance.connection.host);
 
   } catch (error) {
