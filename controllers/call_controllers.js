2eb8539ff5b172b26f0dc0fc458f2d1c87e22ee2
Rsoniie <roshansoniee@gmail.com>
2025-01-11 14:00:53 +0530

    All task Done only deployment left

--- controllers/call_controllers.js
+++ controllers/call_controllers.js
@@ -67,11 +67,65 @@ const Info = async(req, res) =>
 }
 };
 
+const Stats = async(req, res) => {
+    try{
+        const {coin} = await req.body;
+        console.log("This is the requested currency", coin);
+
+        const latestDocument = await Coininfo.findOne({}, {}, { sort: { createdAt: -1 } });
+
+        if(!latestDocument)
+        {
+            return res.status(500).json({message: "No latest data found"});
+        }
+
+        // console.log("This is the latest data we got", latestDocument);
+
+        const response = latestDocument[coin];
+        console.log(response);
+
+        return res.status(200).json({response});
+    }catch(error)
+    {
+        console.log("This is error in catch block", error);
+
+        return res.status(500).json({message:"Internal Server error"});
+    }
+};
+
+const Deviation = async(req, res) => {
+    try{
+      const {coin} = await req.body;
+      const number_list = await Coininfo.find({})
+      .sort({ createdAt: -1 })
+      .limit(100);
+
+      if(number_list.length == 0)
+      {
+        return res.status(400).json({message: "No data found"});
+      }
+
+      let prices = [];
+
+      for(let i = 0; i < number_list.length; i++)
+      {
+        prices.push(number_list[i][coin].current_price);
+      }
+
+      const mean = prices.reduce((sum, price) => sum + price, 0) / prices.length;
+      const squaredDifferences = prices.map(price => Math.pow(price - mean, 2));
+      const variance = squaredDifferences.reduce((sum, diff) => sum + diff, 0) / prices.length;
+      const deviation = Math.sqrt(variance);
+      console.log("This is latest hundred documents", deviation);
+      return res.status(200).json({deviation});
+    }
+    catch(error)
+    {
+        console.log("This is the error in catch block", error);
+        return res.status(500).json({message: "Internal server error"});
+    }
+};
 
 
 
-
-
-
-
-export {Info};
+export {Info, Stats, Deviation};
--- routes/call_routes.js
+++ routes/call_routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { Info } from "../controllers/call_controllers.js";
+import { Info, Stats, Deviation } from "../controllers/call_controllers.js";
 const Inforoute = Router();
 
 
 Inforoute.post('/info', Info);
+
+Inforoute.get('/stats', Stats);
+
+Inforoute.get('/deviation', Deviation);
 
 
 export default Inforoute;
